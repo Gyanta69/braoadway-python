9a8fcec958f95c91949ac60ea19cc36b780558ef
Pragyan Khadka <khadkapragyan84@gmail.com>
2024-02-25 07:48:39 +0545

    dataType change

--- datatypes.py
+++ datatypes.py
@@ -3,3 +3,16 @@
 int
 float
 complex
+# a = 34.2
+# b =2.5
+# c = 2 + 5j
+# print(type(a))
+
+# datatype conversion
+
+input_number = input("Enter a number")
+print (type(input_number))
+input_number = int(input_number)
+print(type(input_number))
+input_number = float(input_number)
+print(type(input_number))
