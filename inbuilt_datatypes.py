44dbfe4d629915341348785f4dd8494a8f480c27
Pragyan Khadka <khadkapragyan84@gmail.com>
2024-02-27 21:42:45 +0545

    datatypes

--- inbuilt_datatypes.py
+++ inbuilt_datatypes.py
@@ -39,5 +39,32 @@ color = ["red", "Green", "Yellow", "Black", "white"]
 color.insert(0,"Black")
 print(color)
 
+# to concade a list
+a = [1,2,3,4,5]
+b = [3,4,5,6,7,8]
+# len is used to calculate the length of a list
+print(len(a))
+print(a+b)
 
+a = [] # empty list 
+print(type(a))
+print(len(a))
+
+# extend
+color = ["red", "Green", "Yellow", "Black", "white"]
+extendcolor = ["Violet","Haze","Indigo"]
+color.extend(extendcolor) # extend method is used add two different variable inside a list  
+print(color)
+
+
+# ADDING VALUES IN A LISTS
+empty_list = []
+print("Ans before adding in list:",empty_list)
+a = input("Enter a number ")
+b = input("Enter a number ")
+c = input("Enter a number ")
+empty_list.append(a)
+empty_list.append(b)
+empty_list.append(c)
+print("Ans after adding in list:",empty_list)
 
