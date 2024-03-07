660e4419088f3020b79ce241aad56804e260e45b
Pragyan Khadka <khadkapragyan84@gmail.com>
2024-03-07 06:45:12 +0545

    Update code

--- electricity.py
+++ electricity.py
@@ -1,11 +1,17 @@
-unit = float("Input Electrical unit charges")
-if(unit <= 50):
-    print(amt = unit * 0.50)
-elif(unit <= 150):
-     print( amt = 25 + ((unit-50) * 0.75))
-elif(unit <=200):
-     print(amt = 100 + ((unit-150) * 1.20))  
-else:
-     print(amt = 220 + ((unit-250) * 1.50))  
+units = int(input("Enter the number of units consumed: "))
 
-total = amt * 0.20
+if units <= 50:
+    bill = units * 0.5
+elif units <= 150:
+    bill = 25 + (units - 50) * 0.75
+elif units <= 250:
+    bill = 25 + 75 + (units - 150) * 1.2
+else:
+    bill = 25 + 75 + 120 + (units - 250) * 1.5
+
+surcharge = 0.2 * bill
+total_bill = bill + surcharge
+
+print("Electricity Bill: Rs.", bill)
+print("Surcharge (20%): Rs.", surcharge)
+print("Total Bill: Rs.", total_bill)
