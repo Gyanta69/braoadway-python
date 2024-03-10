a400ac9e4569686aa60a0c721d3c9d87af471a90
Pragyan Khadka <khadkapragyan84@gmail.com>
2024-03-10 11:37:40 +0545

    Loop in python

---
+++ loop.py
@@ -0,0 +1,53 @@
+# loop
+# for loop
+# for loop used in string , list dict and so on
+a = [1,2,3,4,5]
+for i in a :
+    print(i)
+for i in "broadway":
+    print(i)
+
+# loops  list of string
+a = ["Prayan","khadka","Budhanilkantha"]
+for i in a :
+    print(i)
+
+# set data
+set_data = {1,2,3,4,5,6,6,7,3}
+for i in set_data:
+    print(i)
+
+a = [1,2,3,4,5]
+for i in a :
+    if(i==1):
+        print("1 is found")
+
+#range
+for i in range(2,12,2):
+    print(i)
+
+
+for i  in range (0,6,1):
+        print(i)
+
+a = int(input("Enter a number"))
+if(a==2):
+     for a in range(0,10,2):
+          print(a)
+elif(a==3):
+     for a in range(0,10,3):
+          print(a)
+
+# table of 2
+for i in range(1,11,1):
+     result =f'2 x {i} = {2*i}'
+     print(result)
+
+
+i = int(input("Enter a number"))
+for i in range(1,11,1):
+     result =f'2 x {i} = {2*i}'
+     print(result)
+
+# range(1=startvalue,3=endvalue,1=stepvalue)
+# while loop
