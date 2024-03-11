ddf5a7eec9f85e1559578467e231627e0097e8fb
Pragyan Khadka <khadkapragyan84@gmail.com>
2024-03-11 22:56:06 +0545

    upadate loops in python

--- loop.py
+++ loop.py
@@ -23,6 +23,7 @@ for i in a :
         print("1 is found")
 
 #range
+ # range(1=startvalue,3=endvalue,1=stepvalue)
 for i in range(2,12,2):
     print(i)
 
@@ -30,7 +31,7 @@ for i in range(2,12,2):
 for i  in range (0,6,1):
         print(i)
 
-a = int(input("Enter a number"))
+a =int(input("Enter a number"))
 if(a==2):
      for a in range(0,10,2):
           print(a)
@@ -43,11 +44,32 @@ for i in range(1,11,1):
      result =f'2 x {i} = {2*i}'
      print(result)
 
+#to ask a user input and show it's table
+num = int(input("Enter a number: "))
 
-i = int(input("Enter a number"))
-for i in range(1,11,1):
-     result =f'2 x {i} = {2*i}'
-     print(result)
+for i in range(1, 11):
+    result = f"{num} x {i} = {num * i}"
+    print(result)
 
-# range(1=startvalue,3=endvalue,1=stepvalue)
-# while loop
+#append
+    a = []
+    for i in range(1,11):
+        a.append(i)
+    print(a)
+
+language =['python','c++','django','swift','PHP','pandas']
+for i in range(0,len(language),1):
+     print(language[i])
+
+# nested loop
+     for i in ['broadway','python']:
+          for j in range(0,1,1):
+               print(i,j)
+
+for i in range(1,3,1):
+     print(i)
+     for j in range(1,10,1):
+          if j == 2:
+               break
+          print("j ko value",j)
+          
